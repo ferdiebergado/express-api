0e1c2258c2d21e069c12132182c23fbd29c5485e
ferdie <ferdiebergado@gmail.com>
2022-07-24 20:06:25 +0800

    Formatted with prettier

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,27 @@
-import express from "express";
-import authRouter from "./lib/auth/auth.router";
+import express from 'express'
+import authRouter from './lib/auth/auth.router'
 import {
-  errorHandler,
-  notFoundHandler,
-  requestLogger,
-  homeHandler,
-} from "./lib/middlewares";
+    errorHandler,
+    notFoundHandler,
+    requestLogger,
+    homeHandler,
+} from './lib/middlewares'
 
-const app = express();
+const app = express()
 
 // express settings
-app.disable("x-powered-by");
+app.disable('x-powered-by')
 
 // middlewares
-app.use(express.json());
-app.use(requestLogger);
+app.use(express.json())
+app.use(requestLogger)
 
 // app routes
-app.get("/", homeHandler);
-app.use("/auth", authRouter);
+app.get('/', homeHandler)
+app.use('/auth', authRouter)
 
 // error handlers
-app.all("*", notFoundHandler);
-app.use(errorHandler);
+app.all('*', notFoundHandler)
+app.use(errorHandler)
 
-export default app;
+export default app
