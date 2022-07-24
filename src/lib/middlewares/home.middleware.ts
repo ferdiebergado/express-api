979b3e2c5e1c766a326af7abf9227ae401abae6a
ferdie <ferdiebergado@gmail.com>
2022-07-24 20:05:29 +0800

    Changed any to Record

--- src/lib/middlewares/home.middleware.ts
+++ src/lib/middlewares/home.middleware.ts
@@ -1,16 +1,19 @@
-import { Request, Response, NextFunction } from "express";
-import { parsePackageJson } from "../helpers";
+import { Request, Response, NextFunction } from 'express'
+import { parsePackageJson } from '../helpers'
 
 export const homeHandler = async (
-  _req: Request,
-  res: Response,
-  next: NextFunction
+    _req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  try {
-    const { name, version } = (await parsePackageJson()) as any;
+    try {
+        const { name, version } = (await parsePackageJson()) as Record<
+            string,
+            unknown
+        >
 
-    res.json({ name, version, status: "up" });
-  } catch (err) {
-    next(err);
-  }
-};
+        res.json({ name, version, status: 'up' })
+    } catch (err) {
+        next(err)
+    }
+}
