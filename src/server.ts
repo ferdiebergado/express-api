7e4c9ae06f2683b87ec76409c57eae657f6dfa6c
ferdie <ferdiebergado@gmail.com>
2022-07-24 20:07:27 +0800

    Formatted with prettier

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import http from "http";
-import pool from "./db";
-import app from "./app";
+import http from 'http'
+import db from './db'
+import app from './app'
 
-const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
+const server = http.createServer(app)
+const PORT = process.env.PORT || 3000
 
 const start = async () => {
-  await pool.getConnection();
+    await db.getConnection()
 
-  console.log("Connected to the database.");
+    console.log('Connected to the database.')
 
-  server.listen(PORT, () =>
-    console.log("Server listening on port " + PORT + "...")
-  );
-};
+    server.listen(PORT, () =>
+        console.log('Server listening on port %d...', PORT)
+    )
+}
 
-start();
+start()
